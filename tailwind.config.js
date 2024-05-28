4ec356a157516548db5ed560f952ad9ccbcd8e6e
ajeer-alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2024-05-29 01:41:50 +0300

    chore: remove .css files and install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif'],
+                girdTemplateColumns: {
+                    '70/30': '70% 28%',
+                }
+            }
+        },
+    },
+    plugins: [],
+}
