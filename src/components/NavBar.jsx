98edead66647c9e5a71ac8e8770ab84a2e531451
ajeer-alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2024-05-31 04:45:39 +0300

    chore: navbar icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,41 @@
 import Logo from '../assets/Screenshot_from_2024-05-29_02-22-25-removebg-preview.png'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-    const activeClass=({isActive})=>
-        isActive ?"bg-black text-white bg-yellow-500 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" :"text-white bg-yellow-500hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+    const activeClass = ({ isActive }) =>
+        isActive ? "bg-black text-white bg-yellow-500 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
 
     return (
         <div>
             <nav className="bg-blue-700 border-b border-blue-500">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="flex h-20 items-center justify-between">
-                        <div
-                            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-                        >
-                            {/* <!-- Logo --> */}
+                        <div className="flex flex-1 items-center justify-center sm:justify-start">
+                            {/* Logo */}
                             <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
                                 <img
                                     className="h-14 w-auto"
                                     src={Logo}
                                     alt="Promising Jobs"
                                 />
-                                <span className="hidden md:block text-white text-2xl font-bold ml-2"
+                                <span className="hidden sm:block text-white text-2xl font-bold ml-2"
                                 >Promising Jobs</span
                                 >
                             </NavLink>
-                            <div className="md:ml-auto">
+                            {/* Hamburger Menu - visible on smaller screens */}
+                            <div className="sm:hidden flex items-center">
+                                <button className="text-white hover:text-gray-200 focus:outline-none focus:text-gray-200">
+                                    {/* Icon for menu (using heroicons, for example) */}
+                                    <svg className="h-6 w-6 fill-current" viewBox="0 0 20 20"><path fillRule="evenodd" d="M3 5h14v2H3V5zm0 6h14v2H3v-2zm0 6h14v2H3v-2z" clipRule="evenodd"></path></svg>
+                                </button>
+                            </div>
+                            {/* Navigation Links */}
+                            <div className="hidden sm:block md:ml-auto">
                                 <div className="flex space-x-2">
-                                    <NavLink
-                                        to="/"
-                                        className={activeClass}
-                                    >Home</NavLink
-                                    >
-                                    <NavLink
-                                        to="/jobs"
-                                        className={activeClass}
-                                    >Jobs</NavLink
-                                    >
-
-                                    <NavLink
-                                        to="/contact"
-                                        className={activeClass}
-                                    >Contact us</NavLink
-                                    >
-                                    <NavLink
-                                        to="/add-job"
-                                        className={activeClass}
-                                    >Add Job</NavLink
-                                    >
+                                    <NavLink to="/" className={activeClass}>Home</NavLink>
+                                    <NavLink to="/jobs" className={activeClass}>Jobs</NavLink>
+                                    <NavLink to="/contact" className={activeClass}>Contact us</NavLink>
+                                    <NavLink to="/add-job" className={activeClass}>Add Job</NavLink>
                                 </div>
                             </div>
                         </div>
--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 
 const ViewAllJobs = () => {
   return (
-    <section className="m-auto max-w-lg my-10 px-6">
+    <section className="m-auto max-w-lg my-10 px-6 ">
       <Link
         to="/jobs"
         className="block bg-yellow-500 text-white text-center py-4 px-6 rounded-xl hover:bg-yellow-600"
